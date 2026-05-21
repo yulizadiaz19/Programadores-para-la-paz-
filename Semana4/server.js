e60b6bef5eda3b66433279b6d23980f6fa0c0dde
yulizadiaz19 <diazyuliza336@gmail.com>
2026-05-21 14:24:36 -0500

    Create server.js

---
+++ Semana4/server.js
@@ -0,0 +1,11 @@
+JavaScript
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Servidor funcionando correctamente');
+});
+
+app.listen(3000, () => {
+    console.log('Servidor ejecutándose en puerto 3000');
+});
